18989d83dfd1c227b176353f6d7c318c232490f2
andrew sosa <andrespeaker12@gmail.com>
2026-06-09 15:20:38 -0500

    initial proyect

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Toaster } from "react-hot-toast";
+import { AppRoutes } from "./routes/routes";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <AppRoutes />
+      <Toaster position="top-right" />
+    </>
+  );
+}
+
+export default App;
