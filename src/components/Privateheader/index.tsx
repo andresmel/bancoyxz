922e103e61eac739e5c4c516d4a929045e1da6b5
andrew sosa <andrespeaker12@gmail.com>
2026-06-13 09:26:19 -0500

    fix(9 onboarding): fixes in onboarding and more css styles

--- src/components/Privateheader/index.tsx
+++ src/components/Privateheader/index.tsx
@@ -9,31 +9,31 @@ export const PrivateHeader = () => {
     logout();
     navigate("/login");
   };
-  console.log("User in PrivateHeader:", user);
+
   return (
     <>
-      <nav className="relative z-20 flex items-center justify-between flex-wrap bg-sky-700 p-3 shadow-[0_10px_24px_rgba(107,114,128,0.35)]">
-        <div>
+      <nav className="relative flex items-center justify-between flex-wrap bg-sky-700 shadow-[0_10px_24px_rgba(107,114,128,0.35)]">
+        <div className="p-[2vh] bg-sky-900">
           <LogoBanck />
         </div>
         <div className="flex items-center">
-          <span className="text-white text-xl tracking-tight flex items-center">
-            <UserRound className="mr-2" /> Bienvenido, {user?.name}
+          <span className="text-white text-xl tracking-tight flex items-center p-[2vh]  bg-sky-600">
+            <UserRound className="mr-2" /> {user?.name}
           </span>
-          <span className="text-white text-xl flex items-center ml-6">
+          <span className="text-white text-xl flex items-center pt-[2vh] p-[2vh]   bg-sky-800">
             <Mail className="mr-2" />
             {user?.email}
           </span>
 
           <button
             onClick={handleLogout}
-            className="ml-6  hover:bg-slate-500 text-white text-gray py-2 text-xl px-4 rounded flex items-center"
+            className="  hover:bg-slate-500 text-white text-gray p-[2vh]  text-xl px-4 rounded flex items-center bg-sky-900"
           >
             <LogOut className="mr-2" /> cerrar sesion
           </button>
 
-          <div className="ml-4 rounded-full overflow-hidden">
-            <img src={`https://ui-avatars.com/api/?name=${user?.name}&background=0D8ABC&color=fff&size=50`} alt="Avatar" className="w-10 h-10 rounded-full" />
+          <div className="mr-2 ml-3 rounded-full overflow-hidden ">
+            <img src={`https://ui-avatars.com/api/?name=${user?.name}&background=0D8ABC&color=fff&size=50`} title={user?.name} alt="Avatar" className="w-10 h-10 rounded-full" />
           </div>
         </div>
       </nav>
